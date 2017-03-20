c3b7fdd336a8376ae0bc463422c674d0fa5e085f
apu <pasha_anik@ukr.net>
2017-03-20 15:33:35 +0200

    Add to routing table for previous commits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { UsersComponent }      from './users.component';
 import { UserDetailComponent }  from './user-detail.component';
 import { ProductsComponent }      from './products.component';
 import { BasketComponent }      from './basket.component';
+import { CheckoutComponent }   from './checkout.component';
+import {InvoiceComponent} from "./invoice.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: 'detail/:id', component: UserDetailComponent },
   { path: 'users',     component: UsersComponent },
   { path: 'products',  component: ProductsComponent },
-  { path: 'basket',  component: BasketComponent }
+  { path: 'basket',  component: BasketComponent },
+  { path: 'checkout',  component: CheckoutComponent },
+  { path: 'invoice',  component: InvoiceComponent }
 ];
 
 @NgModule({
